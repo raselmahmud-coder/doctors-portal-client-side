fb735b033a568caacd4ab906d963fb1ef4c8733f
RASEL MAHMUD <raselmahmud98262@gmail.com>
2022-05-13 18:01:27 +0800

    firebase init

--- src/Components/Pages/Appointment/AvailableAppointment/AvailableAppointment.js
+++ src/Components/Pages/Appointment/AvailableAppointment/AvailableAppointment.js
@@ -7,7 +7,7 @@ const AvailableAppointment = ({ selected }) => {
   const [Bookings, setBookings] = useState([]);
   const [treatment, setTreatment] = useState(null);
   useEffect(() => {
-    fetch(`fakeService.json`)
+    fetch(`http://localhost:5000/services`)
       .then((res) => res.json())
       .then((data) => setBookings(data));
   }, []);
